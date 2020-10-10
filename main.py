eee8944f0a4af322c207b8e2d9197672f1a7bfde
yuzuki-aritomo <yuzuki.aritomo@gmail.com>
2020-10-10 19:27:39 +0900

    修正

--- main.py
+++ main.py
@@ -30,24 +30,24 @@ def callback():
         abort(400)
     return "OK"
 
-@handler.add(MessageEvent,message=TextMessage)
-def handle_message(event):
-    # profile情報の取得
+@handler.add(MessageEvent, message=TextMessage)
+def response_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
+
     status_msg = profile.status_message
     if status_msg != "None":
         # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
         status_msg = "なし"
 
     messages = TemplateSendMessage(alt_text="Buttons template",
-                                    emplate=ButtonsTemplate(
+                                    template=ButtonsTemplate(
                                         thumbnail_image_url=profile.picture_url,
                                         title=profile.display_name,
                                         text=f"User Id: {profile.user_id[:5]}...\n"
                                             f"Status Message: {status_msg}",
                                         actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
-    line_bot_api.reply_message(event.reply_token, messages=messages)
 
+    line_bot_api.reply_message(event.reply_token, messages=messages)
     # line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
 if __name__=="__main__":
